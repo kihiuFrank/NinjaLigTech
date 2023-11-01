bf838a3cd6484d47f82281c12cd6b6b7f48f1e15
kihiuFrank <franklinekihiu@gmail.com>
2023-11-02 01:44:14 +0300

    design changes

--- app/components/Navbar/Drawerdata.tsx
+++ app/components/Navbar/Drawerdata.tsx
@@ -8,15 +8,15 @@ interface NavigationItem {
 }
 
 const navigation: NavigationItem[] = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Courses', href: '#courses', current: false },
-  { name: 'Mentor', href: '#mentor', current: false },
-  { name: 'Group', href: '#/', current: false },
-  { name: 'Testimonial', href: '#testimonial', current: false },
-]
+  { name: "Home", href: "/", current: true },
+  { name: "Courses", href: "#courses", current: false },
+  { name: "Mentor", href: "#mentor", current: false },
+  { name: "Group", href: "#/", current: false },
+  { name: "Testimonial", href: "#testimonial", current: false },
+];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const Data = () => {
@@ -30,10 +30,12 @@ const Data = () => {
                 key={item.name}
                 href={item.href}
                 className={classNames(
-                  item.current ? 'text-black hover:opacity-100' : 'hover:text-black hover:opacity-100',
-                  'py-1 text-lg font-normal opacity-75 block'
+                  item.current
+                    ? "text-black hover:opacity-100"
+                    : "hover:text-black hover:opacity-100",
+                  "py-1 text-lg font-normal opacity-75 block"
                 )}
-                aria-current={item.current ? 'page' : undefined}
+                aria-current={item.current ? "page" : undefined}
               >
                 {item.name}
               </Link>
@@ -50,6 +52,6 @@ const Data = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Data;
