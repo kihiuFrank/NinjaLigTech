d0d6649b351051b0e33010774d2e343f0fd1eff3
kihiuFrank <franklinekihiu@gmail.com>
2023-11-06 09:33:45 +0300

    auth state change

--- components/ProtectedRoute/ProtectedRoute.tsx
+++ components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,12 +2,13 @@
 import { auth } from "@/config/firebase.config";
 import { useAuth } from "../../context/AuthContext";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
-  const { user } = useAuth();
+  //const { user } = useAuth();
+  const [user, setUser] = useState<any>(null);
 
   // useEffect(() => {
   //   if (!user.uid) {
@@ -17,11 +18,14 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     onAuthStateChanged(auth, (user: any) => {
-      if (!user) {
+      if (user) {
+        setUser(auth.currentUser);
+      } else {
+        setUser(null);
+        router.push("/login");
         console.error(
           "Access to protected route denied, redirecting to login..."
         );
-        router.push("/login");
       }
     });
   }, [router, user]);
